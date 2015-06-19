eba4c40982d687182f0a2918c7aedd617816fd23
etkirsch <ekirsch@allegient.com>
2015-06-18 21:31:42 -0400

    Need to figure out how to procedurally generate down for world and such

---
+++ Extensions/Fantasy World/Lifeforms/Macro/Generation Parameters/BasicPlantGenerationParameters.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PGE.Core.Generated_Items;
+
+namespace PGE.Fantasy_World.Lifeforms.Macro.Generation_Parameters
+{
+    public class BasicPlantGenerationParameters : GenerationParameters
+    {
+    }
+}
