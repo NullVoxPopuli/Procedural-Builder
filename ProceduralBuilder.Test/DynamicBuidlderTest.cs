f304cfec911288738f3ea6716c1dba38dac3bdd2
L. Preston Sego III <LPSego3+dev@gmail.com>
2015-08-05 13:31:40 -0400

    inprgress method invocation tests

---
+++ ProceduralBuilder.Test/DynamicBuidlderTest.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.CSharp.RuntimeBinder;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProceduralBuilder.Test.Objects;
+
+namespace ProceduralBuilder.Test
+{
+    [TestClass]
+    public class DynamicBuidlderTest
+    {
+
+        [TestMethod]
+        public void MethodInvocation_OfField()
+        {
+            dynamic parentBuilder = new DynamicParentBuilder();
+            var parent = parentBuilder
+                .WithRangeMultiplier(2)
+                .Build();
+
+            Assert.AreEqual(
+                actual: parent.RangeMultiplier,
+                expected: 2);
+        }
+
+        [TestMethod]
+        public void MethodInvocation_OfNonField()
+        {
+            dynamic parentBuilder = new DynamicParentBuilder();
+            var parentBuilderType = parentBuilder.GetType();
+
+            Assert.AreEqual(
+                actual: parentBuilderType,
+                expected: typeof(DynamicParentBuilder));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(RuntimeBinderException))]
+        public void MethodInvocation_OfNonExistantMethod()
+        {
+            dynamic parentBuilder = new DynamicParentBuilder();
+            parentBuilder.WithNonExistantMethod(null);
+        }
+
+        [TestMethod]
+        public void MethodInvocation_OfBoolean()
+        {
+
+        }
+    }
+}
